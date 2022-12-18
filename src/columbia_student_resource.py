27c370f47ab4d6ba9c6facd7859c8bf13b8c1080
Helen962 <mx2257@columbia.edu>
2022-12-17 23:20:02 -0500

    Update columbia_student_resource.py

--- src/columbia_student_resource.py
+++ src/columbia_student_resource.py
@@ -90,18 +90,33 @@ class ColumbiaStudentResource:
         return res
 
     @staticmethod
-    def update_by_key(key, data):
-        last_name = data.get('last_name', '')
-        first_name = data.get('first_name', '')
-        middle_name = data.get('middle_name', '')
-        email = data.get('email', '')
-        school_code = data.get('school_code', '')
-        sql = f'UPDATE f22_databases.columbia_students  \
-              SET last_name = \'{last_name}\', first_name = \'{first_name}\', middle_name = \'{middle_name}\', email = \'{email}\', school_code = \'{school_code}\' \
-              WHERE guid=%s;'
+    def update_by_key(id, data):
+        column_name = []
+        result_list = []
+        set_length = len(data)
+        if set_length == 0:
+            set_str = ""
+        else:
+            set_str = ", ".join(["{} = %s"] * set_length)
+            for i in data.items():
+                column_name.append(str(i[0]))
+                result_list.append(str(i[1]))  # type  attention!
+
+        # where
+        where_str = " where guid = %s"
+        result_list.append(id)
+
+        arguments = tuple(result_list)
+        sql_row = "UPDATE f22_databases.columbia_students" + " set " + set_str + where_str
+        sql = sql_row.format(*column_name)
         conn = ColumbiaStudentResource._get_connection()
-        cur = conn.cursor()
-        res = cur.execute(sql, args=key)
-        conn.commit()
-        conn.close()
-        return res
+        cursor = conn.cursor()
+        res = cursor.execute(sql, arguments)
+
+        if res == 1:
+            result = cursor.fetchone()
+            cursor.close()
+            return 1
+        else:
+            cursor.close()
+            return 0
